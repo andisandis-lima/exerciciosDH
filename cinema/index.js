22727a9014dd8864694e4fa3decf4cae5672b45b
andisandis-lima <anderson.ls1998@gmail.com>
2021-09-29 18:48:31 -0300

    resolucoes dos antigos exercicios e tentativa dos outros

--- cinema/index.js
+++ cinema/index.js
@@ -1,20 +1,54 @@
-let movies = [{
-    codigo:01,
-    titulo: 'avatar',
-    duracao: 160,
-    atores: ["Steve","Joana","Marc"],
-    anoDeLancamento: 2018,
-    emCartaz: true
-    
-},
-{
-    codigo:02,
-    titulo: 'Fuga das Galinhas',
-    duracao: 190,
-    atores: ["Pintadinha", "Lucy", "Mary"],
-    anoDeLancamento: 2016,
-    emCartaz: false
-}];
+let movies = require('../database/catalogo.json');
+
+function createMovie(codigo, titulo, duracao, atores, anoDeLancamento, emCartaz) {
+    return {
+        codigo: codigo,
+        titulo: titulo,
+        duracao: duracao,
+        atores: atores,
+        anoDeLancamento : anoDeLancamento,
+        emCartaz: emCartaz,
+    }
+}
+function addMovie(movie) {
+    movies.push(movie);
+}
+
+
+function getMovieById(codigo) {
+    // solução com callback
+    // movies.find(movie => movie.id === id);
+    for(i = 0; i < movies.length; i++) {
+        if(movies[i].codigo == codigo) {
+            return movies[i];
+        }
+    }
+}
+
+
+function changeStatus(codigo) {
+    let movie = getMovieById(codigo);
+    movie.emCartaz = movie.emCartaz ? false : true;
+    return movie;
+}
+
+function listarTodosOsFilmes(movies) {
+    for(i = 0; i < i.length; i++)
+        return movies[i];
+}
+
+function listarFilmesEmCartaz(movies) {
+    for(i = 0; i < i.length; i++) 
+        if (movies.emCartaz == true) {
+            return movies[i]
+        }
+}
+
+module.exports = {movies, createMovie, addMovie, getMovieById, changeStatus, 
+    listarTodosOsFilmes, listarFilmesEmCartaz};
+
+/*
+CRIANDO FILME
 
 function AdcFilme (codigo, titulo, duracao, atores, anoDeLancamento, emCartaz) {
     this.codigo = codigo,
@@ -30,26 +64,16 @@ let terceiroFilme = new AdcFilme (03, "Space Jam", 203,
 
 movies.push(terceiroFilme);
 
-console.log(movies)
-/*
-function busca (movies) {
-    movies.codigo == 01;
-    if (emCartaz == true){
-        emCartaz = false
-    } else if (emCartaz == false){
-        emCartaz = true
-    }
-    return
-};
-
-console.log(movies.find(busca));
+console.log(movies);
 */
+
+
 /*
-Buscando Filme
+BUSCANDO FILME
 
 function busca (movies) {
     return movies.codigo == 01;
 };
 
 console.log(movies.find(busca));
-module.exports = movies*/
+*/
--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-const movies = require('./cinema');
+var {movies, createMovie, addMovie, getMovieById, changeStatus, 
+   listarTodosOsFilmes, listarTodosOsFilmes, listarFilmesEmCartaz} = require('./cinema');
 
-console.log(movies)
+ /*
+ const newMovie = {
+    codigo: 3,
+    titulo: 'Deadpool',
+    duracao: 203,
+    atores: ["Lebrom James", "Damian Lillard", "Anthony Davis"],
+    anoDeLancamento: 2021,
+    emCartaz: false,
+ }
+ */
+
+// criando novo objeto de movie
+let newMovie = createMovie(03, "Space Jam", 203, 
+["Lebrom James", "Damian Lillard", "Anthony Davis"], 2021, true);
+addMovie(newMovie);
+console.log('--------------------------------------------')
+//console.log(movies);
+
+// alterando status
+changeStatus(3);
+console.log('--------------------------------------------')
+
+
+// obtendo objeto de movie pelo id
+let pesquisa = getMovieById(02);
+//console.log(pesquisa)
+
+listarTodosOsFilmes( );
+
+listarFilmesEmCartaz( );
